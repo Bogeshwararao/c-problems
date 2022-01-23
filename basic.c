bae3a6a4032dbb716ce76dad7cc3c1313dd5ac27
bogeshwararao <bogeshwararao12042004@gmail.com>
2022-01-23 11:01:01 +0530

    commit

--- basic.c
+++ basic.c
@@ -238,6 +238,32 @@ printf("\nRollno:%d, Name:%s",st[i].rollno,st[i].name);
 }
 return 0;
 }
+// PASSING STRUCTURE TO FUNCTION IN C BY ADDRESS
+#include <stdio.h> 
+#include <string.h>
+struct student
+{
+int id;
+char name[20]; 
+float percentage;
+};
+void func(struct student *record);
+int main()
+{
+struct student record;
+record.id=1; 
+strcpy(record.name, "Raju"); 
+record.percentage = 86.5;
+func(&record); 
+return 0;
+}
+void func(struct student *record)
+{
+printf(" Id is: %d \n", record->id); 
+printf(" Name is: %s \n", record->name);
+printf(" Percentage is: %f \n", record->percentage);
+}
+
 
 
 
